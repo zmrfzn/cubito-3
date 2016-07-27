2b40244f7c6ff7d1d58564bbf17726cae2efefcb
zameer <zmrfzn@gmail.com>
2016-07-28 02:06:53 +0530

    created basic view in app

--- cubito/trips/views.py
+++ cubito/trips/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from urllib2 import urlopen
+from django.http import HttpRequest,HttpResponse
+import json
+from time import sleep
 
 # Create your views here.
+def index(request):
+	trip_id = json.load(urlopen("http://cubito.co.in/assignment/gpslocation.php"))
+	return HttpResponse(trip_id['trip_id'])
