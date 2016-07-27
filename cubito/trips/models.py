db1d6fdbbeab75af84da301812ca34611210f61c
zameer <zmrfzn@gmail.com>
2016-07-28 02:37:33 +0530

    created models , tested with inserting trip id

--- cubito/trips/models.py
+++ cubito/trips/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class trips(models.Model):
+	id = models.AutoField(primary_key=True)
+	trip_id = models.CharField(max_length=255)
+	trip_data = models.TextField()
--- cubito/trips/views.py
+++ cubito/trips/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from urllib2 import urlopen
 from django.http import HttpRequest,HttpResponse
+from django.db import models
+from trips.models import trips
+from urllib2 import urlopen
 import json
 from time import sleep
 
 # Create your views here.
 def index(request):
 	trip_id = json.load(urlopen("http://cubito.co.in/assignment/gpslocation.php"))
+	db_obj = trips(trip_id=trip_id['trip_id'])
+	db_obj.save()
 	return HttpResponse(trip_id['trip_id'])
